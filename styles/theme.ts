9073e93d1eeec6ffc29639df047570870675ed83
ireshka <ireshka@users.noreply.github.com>
2022-03-28 12:38:51 +0200

    feat: add <AppBar />

--- styles/theme.ts
+++ styles/theme.ts
@@ -4,6 +4,11 @@ let theme = createTheme({
   palette: {
     text: {
       primary: "#6F7781",
+      secondary: "#fff",
+    },
+    grey: {
+      ["300"]: "#6F7781",
+      ["900"]: "#24292F",
     },
   },
 });
@@ -11,6 +16,15 @@ let theme = createTheme({
 theme = createTheme(theme, {
   typography: {
     fontSize: 16,
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: theme.palette.grey["900"],
+        },
+      },
+    },
   },
 });
 
